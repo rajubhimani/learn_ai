abd6060481ed21457d8a4ca644d5f000651e66f8
Raju Bhimani <raju.bhimani07@live.com>
2025-09-26 00:59:41 +0530

    add qdrant

---
+++ vector_db/qdrant/main.py
@@ -0,0 +1,123 @@
+import getpass
+import os
+from uuid import uuid4
+from dotenv import load_dotenv
+
+
+# from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from langchain_core.documents import Document
+
+load_dotenv()
+# if not os.environ.get("OPENAI_API_KEY"):
+#     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+
+def collection_exists(client: QdrantClient, name: str) -> bool:
+    collections = client.get_collections()
+    return any(c.name == name for c in collections.collections)
+
+
+def main():
+    print("Hello from learn-langchain!")
+
+    # embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+    url = "localhost:6334"
+    client = QdrantClient(url=url, prefer_grpc=True)
+    if collection_exists(client=client, name="demo_collection"):
+        client.delete_collection("demo_collection")
+    # client.create_collection(
+    #     collection_name="demo_collection",
+    #     vectors_config=VectorParams(size=3072, distance=Distance.COSINE),
+    # )
+    client.create_collection(
+        collection_name="demo_collection",
+        vectors_config=VectorParams(size=1024, distance=Distance.COSINE),
+    )
+
+    vector_store = QdrantVectorStore(
+        client=client,
+        collection_name="demo_collection",
+        embedding=embeddings,
+    )
+
+    document_1 = Document(
+        page_content="I had chocolate chip pancakes and scrambled eggs for breakfast this morning.",
+        metadata={"source": "tweet"},
+    )
+
+    document_2 = Document(
+        page_content="The weather forecast for tomorrow is cloudy and overcast, with a high of 62 degrees Fahrenheit.",
+        metadata={"source": "news"},
+    )
+
+    document_3 = Document(
+        page_content="Building an exciting new project with LangChain - come check it out!",
+        metadata={"source": "tweet"},
+    )
+
+    document_4 = Document(
+        page_content="Robbers broke into the city bank and stole $1 million in cash.",
+        metadata={"source": "news"},
+    )
+
+    document_5 = Document(
+        page_content="Wow! That was an amazing movie. I can't wait to see it again.",
+        metadata={"source": "tweet"},
+    )
+
+    document_6 = Document(
+        page_content="Is the new iPhone worth the price? Read this review to find out.",
+        metadata={"source": "website"},
+    )
+
+    document_7 = Document(
+        page_content="The top 10 soccer players in the world right now.",
+        metadata={"source": "website"},
+    )
+
+    document_8 = Document(
+        page_content="LangGraph is the best framework for building stateful, agentic applications!",
+        metadata={"source": "tweet"},
+    )
+
+    document_9 = Document(
+        page_content="The stock market is down 500 points today due to fears of a recession.",
+        metadata={"source": "news"},
+    )
+
+    document_10 = Document(
+        page_content="I have a bad feeling I am going to get deleted :(",
+        metadata={"source": "tweet"},
+    )
+
+    documents = [
+        document_1,
+        document_2,
+        document_3,
+        document_4,
+        document_5,
+        document_6,
+        document_7,
+        document_8,
+        document_9,
+        document_10,
+    ]
+    uuids = [str(uuid4()) for _ in range(len(documents))]
+
+    vector_store.add_documents(documents=documents, ids=uuids)
+    # vector_store.delete(ids=[uuids[-1]])
+
+    results = vector_store.similarity_search(
+        "LangChain provides abstractions to make working with LLMs easy", k=2
+    )
+    for res in results:
+        print(f"* {res.page_content} [{res.metadata}]")
+
+
+if __name__ == "__main__":
+    main()
